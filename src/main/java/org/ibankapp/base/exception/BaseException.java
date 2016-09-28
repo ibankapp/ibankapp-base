76840e9ced5a068b85851b5d848522871fd76c9f
王玥 <wangyued@126.com>
2016-09-28 17:38:47 +0800

    添加异常基类,修改dao方法,修改jpa依赖,发布到改到sonatype

---
+++ src/main/java/org/ibankapp/base/exception/BaseException.java
@@ -0,0 +1,67 @@
+package org.ibankapp.base.exception;
+
+
+public class BaseException extends RuntimeException{
+
+
+    private static final long serialVersionUID = 8639994227897113279L;
+
+    private String messageId;
+
+
+    private String message;
+
+    private final static String MESSAGE_FILE = "base_message.properties";
+
+    static {
+        PropertyUtil.load(MESSAGE_FILE);
+    }
+
+
+    public BaseException() {
+    }
+
+    public BaseException(String messageId) {
+        this.messageId = messageId;
+        this.message = PropertyUtil.getProperty(messageId);
+    }
+
+
+    public BaseException(String messageId, String param) {
+        this.messageId = messageId;
+        if (messageId.equals("999999")) {
+            this.message = param;
+        } else {
+            this.message = PropertyUtil.getProperty(messageId, param);
+        }
+
+    }
+
+
+    public BaseException(String messageId, String param1, String param2) {
+        this.messageId = messageId;
+        this.message = PropertyUtil.getProperty(messageId, param1, param2);
+    }
+
+
+    public BaseException(String messageId, String[] params) {
+        this.messageId = messageId;
+        this.message = PropertyUtil.getProperty(messageId, params);
+    }
+
+    @Override
+    public synchronized BaseException initCause(Throwable cause) {
+
+        return (BaseException) super.initCause(cause);
+    }
+
+
+    public String getMessage() {
+        return this.message;
+    }
+
+
+    public String getMessageId() {
+        return this.messageId;
+    }
+}
---
+++ src/main/java/org/ibankapp/base/exception/PropertyUtil.java
@@ -0,0 +1,136 @@
+package org.ibankapp.base.exception;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.text.MessageFormat;
+
+import java.util.Properties;
+
+/**
+ * 配置文件读取类
+ *
+ *
+ * @version        1.0.0, 16/09/27
+ * @author         codelder
+ */
+public class PropertyUtil {
+
+    /** 属性文件对象 */
+    private final static Properties props = new Properties();
+
+    /**
+     * 将配置文件读取进入属性对象
+     *
+     *
+     * @param name 文件路径及名称
+     */
+    public static void load(String name) {
+        InputStream is = null;
+
+        try {
+            is = Thread.currentThread().getContextClassLoader().getResourceAsStream(name);
+            props.load(is);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+
+            throw new BaseException().initCause(e);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            throw new BaseException().initCause(e);
+        } finally {
+            try {
+                if (is != null) {
+                    is.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+
+                throw new BaseException().initCause(e);
+            }
+        }
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @param key
+     *
+     * @return
+     */
+    public static String getProperty(String key) {
+        if (key != null) {
+            return props.getProperty(key);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @param key
+     * @param appendMsg
+     *
+     * @return
+     */
+    public static String getProperty(String key, String appendMsg) {
+        String property = getProperty(key);
+
+        System.out.println(key);
+        System.out.println(property);
+        System.out.println(appendMsg);
+
+        if ((appendMsg != null) && (key != null)) {
+            property = MessageFormat.format(property, appendMsg);
+        }
+
+        return property;
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @param key
+     * @param appendMsgs
+     *
+     * @return
+     */
+    public static String getProperty(String key, String[] appendMsgs) {
+        String property = getProperty(key);
+
+        if ((appendMsgs != null) && (key != null)) {
+            property = MessageFormat.format(property, (Object[]) appendMsgs);
+        }
+
+        return property;
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @param key
+     * @param appendMsg
+     * @param appendMsg1
+     *
+     * @return
+     */
+    public static String getProperty(String key, String appendMsg, String appendMsg1) {
+        String property = getProperty(key);
+
+        if (property == null) {
+            throw new BaseException("E-BASE-000002", key);
+        }
+
+        if ((appendMsg != null) && (key != null)) {
+            property = MessageFormat.format(property, appendMsg, appendMsg1);
+        }
+
+        return property;
+    }
+}
