babe52e16a83d30313e815f6508b99be4027a3df
王玥 <wangyued@126.com>
2017-06-05 20:45:44 +0800

    添加JavaDoc及更改接口去除QueryLint功能

--- base-persistence/src/main/java/org/ibankapp/base/persistence/repository/JpaRepository.java
+++ base-persistence/src/main/java/org/ibankapp/base/persistence/repository/JpaRepository.java
@@ -87,42 +87,160 @@ public interface JpaRepository {
     <T, ID extends Serializable> boolean exist(Class<T> entityClass, ID id);
 
     /**
-     * 查找传入的实体对象类型的所有实体对象
+     * 返回所有查询结果
      *
      * @param entityClass Jpa实体类型
      * @param <T>         Jpa实体类型
-     * @return 所有该实体类型的实体对象List
+     * @return 查询结果List
      */
     <T> List<T> findAll(Class<T> entityClass);
 
+    /**
+     * 分页返回所有查询结果
+     *
+     * @param entityClass Jpa实体类型
+     * @param pageable    分页规则
+     * @param <T>         Jpa实体类型
+     * @return 分页查询结果
+     */
     <T> Page<T> findAll(Class<T> entityClass, Pageable pageable);
 
+    /**
+     * 按照排序规则返回查询结果
+     *
+     * @param entityClass Jpa实体类型
+     * @param sort        排序请求规则
+     * @param <T>         Jpa实体类型
+     * @return 查询结果List
+     */
     <T> List<T> findAll(Class<T> entityClass, Sort sort);
 
+    /**
+     * 按照查询条件返回查询结果
+     *
+     * @param entityClass Jpa实体类型
+     * @param spec        指定的条件对象
+     * @param <T>         Jpa实体类型
+     * @return 查询结果List
+     */
     <T> List<T> findAll(Class<T> entityClass, Specification<T> spec);
 
+    /**
+     * 按照查询条件分页返回查询结果
+     *
+     * @param entityClass Jpa实体类型
+     * @param spec        指定的查询条件
+     * @param pageable    分页规则
+     * @param <T>         Jpa实体类型
+     * @return 分页查询结果
+     */
     <T> Page<T> findAll(Class<T> entityClass, Specification<T> spec, Pageable pageable);
 
+    /**
+     * 按照排序规则、查询条件返回查询结果
+     *
+     * @param entityClass Jpa实体类型
+     * @param spec        查询条件
+     * @param sort        排序规则
+     * @param <T>         Jpa实体类型
+     * @return 查询结果List
+     */
     <T> List<T> findAll(Class<T> entityClass, Specification<T> spec, Sort sort);
 
+    /**
+     * 按照查询条件、排序规则、分页规则返回查询结果
+     *
+     * @param entityClass Jpa实体类型
+     * @param spec        查询条件
+     * @param sort        排序规则
+     * @param pageable    分页规则
+     * @param <T>         Jpa实体类型
+     * @return 分页查询结果
+     */
     <T> Page<T> findAll(Class<T> entityClass, Specification<T> spec, Sort sort, Pageable pageable);
 
-    <T> List<T> findAll(Class<T> entityClass, Specification<T> spec, Sort sort, LockModeType lockMode,
-                        Map<String, Object> queryHints);
+    /**
+     * 按照查询条件，排序规则返回查询结果，并按规则锁定查询结果
+     *
+     * @param entityClass Jpa实体类型
+     * @param spec        查询条件
+     * @param sort        排序规则
+     * @param lockMode    锁模式
+     * @param <T>         Jpa实体类型
+     * @return 查询结果List
+     */
+    <T> List<T> findAll(Class<T> entityClass, Specification<T> spec, Sort sort, LockModeType lockMode);
 
+    /**
+     * 查询满足指定ID集合的数据记录
+     *
+     * @param entityClass Jpa实体类型
+     * @param ids         ID集合
+     * @param isBatch     是否批量查询
+     * @param <T>         Jpa实体类型
+     * @param <ID>        ID类型
+     * @return 查询结果List
+     */
     <T, ID extends Serializable> List<T> findAll(Class<T> entityClass, Iterable<ID> ids, boolean isBatch);
 
+    /**
+     * 返回总记录条数
+     *
+     * @param entityClass Jpa实体类型
+     * @param <T>         Jpa实体类型
+     * @return 记录总条数
+     */
     <T> long count(Class<T> entityClass);
 
+    /**
+     * 返回满足查询条件的记录条数
+     *
+     * @param entityClass Jpa实体类型
+     * @param spec        查询条件
+     * @param <T>         Jpa实体类型
+     * @return 满足条件的记录条数
+     */
     <T> long count(Class<T> entityClass, Specification<T> spec);
 
+    /**
+     * 删除指定ID的数据库记录
+     *
+     * @param entityClass Jpa实体类型
+     * @param id          实体对象ID
+     * @param <T>         Jpa实体类型
+     * @param <ID>        实体对象ID类型
+     */
     <T, ID extends Serializable> void delete(Class<T> entityClass, ID id);
 
+    /**
+     * 从数据库中删除指定实体对象映射的记录
+     *
+     * @param entity Jpa实体类型
+     * @param <T>    Jpa实体类型
+     */
     <T> void delete(T entity);
 
+    /**
+     * 从数据库删除指定实体对象集合映射的所有记录
+     *
+     * @param entities 实体对象集合
+     * @param <T>      Jpa实体类型
+     */
     <T> void delete(Iterable<T> entities);
 
+    /**
+     * 删除所有记录
+     *
+     * @param entityClass Jpa实体类型
+     * @param <T>         Jpa实体类型
+     */
     <T> void deleteAll(Class<T> entityClass);
 
+    /**
+     * 校验bean如果保存到数据库是否满足唯一性要求，不满足抛出相应异常
+     *
+     * @param bean 要检查的游离态实体对象
+     * @param <T>  Jpa实体类型
+     */
     <T> void uniqueValidate(T bean);
 }
