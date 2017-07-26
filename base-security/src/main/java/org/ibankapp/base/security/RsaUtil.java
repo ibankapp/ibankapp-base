732fdb15f93a4a196a6820216eaed9d0c6fb91e3
codelder <codelder@ibankapp.org>
2017-07-26 23:54:04 +0800

    重构Rsa加解密工具类及测试案例

--- base-security/src/main/java/org/ibankapp/base/security/RsaUtil.java
+++ base-security/src/main/java/org/ibankapp/base/security/RsaUtil.java
@@ -10,18 +10,15 @@
 package org.ibankapp.base.security;
 
 import java.io.IOException;
-import java.security.InvalidKeyException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.security.Key;
 import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import net.iharder.Base64;
 
 /**
@@ -37,22 +34,22 @@ public class RsaUtil {
 
   private static final String CIPHER_ALGORITHM = "RSA/ECB/PKCS1Padding";
 
+  private RsaUtil() {
+
+  }
+
   /**
    * 还原公钥，X509EncodedKeySpec 用于构建公钥的规范.
    *
    * @param keyBytes byte[]类型的公钥
    * @return PublicKey类型的公钥
    */
-  private static PublicKey getPublicKey(byte[] keyBytes) throws BaseSecurityException {
+  private static PublicKey getPublicKey(byte[] keyBytes) {
     X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(keyBytes);
     try {
       KeyFactory factory = KeyFactory.getInstance(KEY_ALGORITHM);
       return factory.generatePublic(x509EncodedKeySpec);
-    } catch (NoSuchAlgorithmException e) {
-      e.printStackTrace();
-      throw new BaseSecurityException("E-BASE-SECURITY-000007").initCause(e);
-    } catch (InvalidKeySpecException e) {
-      e.printStackTrace();
+    } catch (Exception e) {
       throw new BaseSecurityException("E-BASE-SECURITY-000007").initCause(e);
     }
   }
@@ -63,218 +60,160 @@ public class RsaUtil {
    * @param keyBytes byte[]类型的私钥
    * @return PrivateKey类型的私钥
    */
-  private static PrivateKey getPrivateKey(byte[] keyBytes) throws BaseSecurityException {
+  private static PrivateKey getPrivateKey(byte[] keyBytes) {
     PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(keyBytes);
     try {
       KeyFactory factory = KeyFactory.getInstance(KEY_ALGORITHM);
       return factory.generatePrivate(pkcs8EncodedKeySpec);
-    } catch (NoSuchAlgorithmException e) {
-      e.printStackTrace();
-      throw new BaseSecurityException("E-BASE-SECURITY-000008").initCause(e);
-    } catch (InvalidKeySpecException e) {
-      e.printStackTrace();
+    } catch (Exception e) {
       throw new BaseSecurityException("E-BASE-SECURITY-000008").initCause(e);
     }
   }
 
   /**
-   * 加密.
+   * 使用RAS算法进行加密.
    *
-   * @param key 公钥
-   * @param plainText 待加密的byte[]类型数据.
-   * @return 加密后的byte[]类型数据
+   * @param keyType 密钥类型
+   * @param skey 密钥
+   * @param clearBytes 要加密的明文
+   * @return 加密后的密文
    */
-  private static byte[] encrypt(PublicKey key, byte[] plainText) throws BaseSecurityException {
+  public static byte[] encrypt(KeyType keyType, String skey, byte[] clearBytes) {
 
+    Key key;
     try {
-      Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
-      cipher.init(Cipher.ENCRYPT_MODE, key);
-      return cipher.doFinal(plainText);
-    } catch (NoSuchAlgorithmException e) {
-      e.printStackTrace();
-      throw new BaseSecurityException("E-BASE-SECURITY-000004").initCause(e);
-    } catch (NoSuchPaddingException e) {
-      e.printStackTrace();
-      throw new BaseSecurityException("E-BASE-SECURITY-000004").initCause(e);
-    } catch (InvalidKeyException e) {
-      e.printStackTrace();
-      throw new BaseSecurityException("E-BASE-SECURITY-000004").initCause(e);
-    } catch (IllegalBlockSizeException e) {
-      e.printStackTrace();
-      throw new BaseSecurityException("E-BASE-SECURITY-000004").initCause(e);
-    } catch (BadPaddingException e) {
-      e.printStackTrace();
-      throw new BaseSecurityException("E-BASE-SECURITY-000004").initCause(e);
+      key = getKey(keyType, skey);
+    } catch (IOException e) {
+      throw new BaseSecurityException("E-BASE-SECURITY-000009");
     }
+
+    return encrypt(key, clearBytes);
   }
 
   /**
    * 加密.
    *
-   * @param key 私钥
-   * @param plainText 待加密的byte[]类型数据
-   * @return 加密后的byte[]类型数据
-   */
-  private static byte[] encrypt(PrivateKey key, byte[] plainText) throws BaseSecurityException {
-
-    try {
-      Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
-      cipher.init(Cipher.ENCRYPT_MODE, key);
-      return cipher.doFinal(plainText);
-    } catch (NoSuchAlgorithmException e) {
-      e.printStackTrace();
-      throw new BaseSecurityException("E-BASE-SECURITY-000004").initCause(e);
-    } catch (NoSuchPaddingException e) {
-      e.printStackTrace();
-      throw new BaseSecurityException("E-BASE-SECURITY-000004").initCause(e);
-    } catch (InvalidKeyException e) {
-      e.printStackTrace();
-      throw new BaseSecurityException("E-BASE-SECURITY-000004").initCause(e);
-    } catch (IllegalBlockSizeException e) {
-      e.printStackTrace();
-      throw new BaseSecurityException("E-BASE-SECURITY-000004").initCause(e);
-    } catch (BadPaddingException e) {
-      e.printStackTrace();
-      throw new BaseSecurityException("E-BASE-SECURITY-000004").initCause(e);
-    }
-  }
-
-  /**
-   * 加密.
-   *
-   * @param keyType 公私密钥类型
-   * @param key 密文
-   * @param plainText 待加密的明文
-   * @return 密文
-   */
-  public static byte[] encrypt(KeyType keyType, String key, byte[] plainText)
-      throws BaseSecurityException {
-    if (keyType == KeyType.PUBLICKEY) {
-      PublicKey publicKey;
-      try {
-        publicKey = getPublicKey(Base64.decode(key));
-      } catch (IOException e) {
-        e.printStackTrace();
-        throw new BaseSecurityException("E-BASE-SECURITY-000004").initCause(e);
-      } catch (BaseSecurityException e) {
-        e.printStackTrace();
-        throw new BaseSecurityException("E-BASE-SECURITY-000004").initCause(e);
-      }
-      return encrypt(publicKey, plainText);
-    } else if (keyType == KeyType.PRIVATEKEY) {
-      PrivateKey privateKey;
-      try {
-        privateKey = getPrivateKey(Base64.decode(key));
-      } catch (IOException e) {
-        e.printStackTrace();
-        throw new BaseSecurityException("E-BASE-SECURITY-000004").initCause(e);
-      } catch (BaseSecurityException e) {
-        e.printStackTrace();
-        throw new BaseSecurityException("E-BASE-SECURITY-000004").initCause(e);
-      }
-      return encrypt(privateKey, plainText);
-    } else {
-      throw new BaseSecurityException("E-BASE-SECURITY-000004");
-    }
-
-  }
-
-  /**
-   * 解密.
-   *
-   * @param key 私钥
-   * @param encodedText 密文
-   * @return 明文
-   */
-  private static String decrypt(PrivateKey key, byte[] encodedText) throws BaseSecurityException {
-    try {
-      Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
-      cipher.init(Cipher.DECRYPT_MODE, key);
-      return new String(cipher.doFinal(encodedText));
-    } catch (NoSuchAlgorithmException e) {
-      e.printStackTrace();
-      throw new BaseSecurityException("E-BASE-SECURITY-000005").initCause(e);
-    } catch (NoSuchPaddingException e) {
-      e.printStackTrace();
-      throw new BaseSecurityException("E-BASE-SECURITY-000005").initCause(e);
-    } catch (InvalidKeyException e) {
-      e.printStackTrace();
-      throw new BaseSecurityException("E-BASE-SECURITY-000005").initCause(e);
-    } catch (IllegalBlockSizeException e) {
-      e.printStackTrace();
-      throw new BaseSecurityException("E-BASE-SECURITY-000005").initCause(e);
-    } catch (BadPaddingException e) {
-      e.printStackTrace();
-      throw new BaseSecurityException("E-BASE-SECURITY-000005").initCause(e);
-    }
-  }
-
-  /**
-   * 通过公钥解密.
-   *
-   * @param key 公钥
-   * @param encodedText 密文
-   */
-  private static String decrypt(PublicKey key, byte[] encodedText) throws BaseSecurityException {
-    try {
-      Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
-      cipher.init(Cipher.DECRYPT_MODE, key);
-      return new String(cipher.doFinal(encodedText));
-    } catch (NoSuchAlgorithmException e) {
-      e.printStackTrace();
-      throw new BaseSecurityException("E-BASE-SECURITY-000005").initCause(e);
-    } catch (NoSuchPaddingException e) {
-      e.printStackTrace();
-      throw new BaseSecurityException("E-BASE-SECURITY-000005").initCause(e);
-    } catch (InvalidKeyException e) {
-      e.printStackTrace();
-      throw new BaseSecurityException("E-BASE-SECURITY-000005").initCause(e);
-    } catch (IllegalBlockSizeException e) {
-      e.printStackTrace();
-      throw new BaseSecurityException("E-BASE-SECURITY-000005").initCause(e);
-    } catch (BadPaddingException e) {
-      e.printStackTrace();
-      throw new BaseSecurityException("E-BASE-SECURITY-000005").initCause(e);
-    }
-  }
-
-  /**
-   * 解密.
-   *
-   * @param keyType 公私密钥类型
    * @param key 密钥
-   * @param encodedText 密文
+   * @param clearBytes 要加密的明文
+   * @return 加密后的密文
    */
-  public static String decrypt(KeyType keyType, String key, byte[] encodedText)
-      throws BaseSecurityException {
+  public static byte[] encrypt(Key key, byte[] clearBytes) {
+    try {
+      Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+      cipher.init(Cipher.ENCRYPT_MODE, key);
+      return cipher.doFinal(clearBytes);
+    } catch (Exception e) {
+      throw new BaseSecurityException("E-BASE-SECURITY-000004").initCause(e);
+    }
+  }
 
-    if (keyType == KeyType.PUBLICKEY) {
-      PrivateKey privateKey;
-      try {
-        privateKey = getPrivateKey(Base64.decode(key));
-      } catch (IOException e) {
-        e.printStackTrace();
-        throw new BaseSecurityException("E-BASE-SECURITY-000005").initCause(e);
-      } catch (BaseSecurityException e) {
-        e.printStackTrace();
-        throw new BaseSecurityException("E-BASE-SECURITY-000005").initCause(e);
-      }
-      return decrypt(privateKey, encodedText);
-    } else if (keyType == KeyType.PRIVATEKEY) {
-      PublicKey publicKey;
-      try {
-        publicKey = getPublicKey(Base64.decode(key));
-      } catch (IOException e) {
-        e.printStackTrace();
-        throw new BaseSecurityException("E-BASE-SECURITY-000005").initCause(e);
-      } catch (BaseSecurityException e) {
-        e.printStackTrace();
-        throw new BaseSecurityException("E-BASE-SECURITY-000005").initCause(e);
-      }
-      return decrypt(publicKey, encodedText);
-    } else {
-      throw new BaseSecurityException("E-BASE-SECURITY-000005");
+  /**
+   * 使用默认字符集进行加密.
+   *
+   * @param keyType 密钥类型
+   * @param skey 密钥字符串
+   * @param clearText 要加密的明文
+   * @return 加密后的密文字符串
+   */
+  public static String encrypt(KeyType keyType, String skey, String clearText) {
+    return encrypt(keyType, skey, clearText, Charset.defaultCharset().name());
+  }
+
+  /**
+   * 加密.
+   *
+   * @param keyType 密钥类型
+   * @param skey 密钥字符串
+   * @param clearText 要加密的明文
+   * @param charset 使用的字符集
+   * @return 加密后的密文字符串
+   */
+  public static String encrypt(KeyType keyType, String skey, String clearText, String charset) {
+    byte[] clearBytes;
+    try {
+      clearBytes = clearText.getBytes(charset);
+    } catch (UnsupportedEncodingException e) {
+      throw new BaseSecurityException("E-BASE-SECURITY-000010");
+    }
+    return Base64.encodeBytes(encrypt(keyType, skey, clearBytes));
+  }
+
+  /**
+   * 解密.
+   *
+   * @param key 密钥
+   * @param cipherBytes 要解密的密文
+   * @return 解密后的明文
+   */
+  public static byte[] decrypt(Key key, byte[] cipherBytes) {
+    try {
+      Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+      cipher.init(Cipher.DECRYPT_MODE, key);
+      return cipher.doFinal(cipherBytes);
+    } catch (Exception e) {
+      throw new BaseSecurityException("E-BASE-SECURITY-000005").initCause(e);
+    }
+  }
+
+  /**
+   * 解密.
+   *
+   * @param keyType 密钥类型
+   * @param skey 密钥字符串
+   * @param cipherBytes 要解密的密文
+   * @return 解密后的明文
+   */
+  public static byte[] decrypt(KeyType keyType, String skey, byte[] cipherBytes) {
+    Key key;
+    try {
+      key = getKey(keyType, skey);
+    } catch (IOException e) {
+      throw new BaseSecurityException("E-BASE-SECURITY-000009");
     }
 
+    return decrypt(key, cipherBytes);
   }
+
+  /**
+   * 使用默认的字符集解密.
+   *
+   * @param keyType 密钥类型
+   * @param skey 密钥
+   * @param cipherText 要解密的密文
+   * @return 解密后的明文
+   */
+  public static String decrypt(KeyType keyType, String skey, String cipherText) {
+    return decrypt(keyType, skey, cipherText, Charset.defaultCharset().name());
+  }
+
+
+  /**
+   * 解密.
+   *
+   * @param keyType 密钥类型
+   * @param skey 密钥字符串
+   * @param cipherText 要解密的密文
+   * @param charset 使用的字符集
+   * @return 解密后明文
+   */
+  public static String decrypt(KeyType keyType, String skey, String cipherText, String charset) {
+    try {
+      byte[] cipherBytes = Base64.decode(cipherText);
+      return new String(decrypt(keyType, skey, cipherBytes), charset);
+    } catch (Exception e) {
+      throw new BaseSecurityException("E-BASE-SECURITY-000010").initCause(e);
+    }
+  }
+
+  private static Key getKey(KeyType keyType, String key) throws IOException {
+    switch (keyType) {
+      case PUBLICKEY:
+        return getPublicKey(Base64.decode(key));
+      case PRIVATEKEY:
+        return getPrivateKey(Base64.decode(key));
+      default:
+        throw new BaseSecurityException("E-BASE-SECURITY-000006");
+    }
+  }
+
 }
--- base-security/src/test/java/org/ibankapp/base/security/test/RsaUtilRightTest.java
+++ base-security/src/test/java/org/ibankapp/base/security/test/RsaUtilRightTest.java
@@ -1,55 +1,104 @@
 package org.ibankapp.base.security.test;
 
+import java.io.UnsupportedEncodingException;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
 import net.iharder.Base64;
 import org.ibankapp.base.security.KeyType;
 import org.ibankapp.base.security.RsaUtil;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class RsaUtilRightTest {
 
-  @Test
-  public void testEncrypt() {
-    new RsaUtil();
+  private String privateStr = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAIetOUUKP3bBQoaqhYb"
+      + "X2IUVpST8iQ9FpV9HAOHWvY6c7tt+ttKtuWACGTF6D3FwoU731TifVPjHZyNWQ115UTtoqQJi89JAZWJ1/pfMaSWN"
+      + "mLQ4wef0M6XTY8A8Kbls6T4ryD1R0aZ03OV4RTgyOIPTWU1t5ulG/+NC7xE+GExxAgMBAAECgYBoMl2IKx2gOz23J"
+      + "GBVtZDAbGYe6J7uDqO5b1M7HesICnfaNA997xMtq47jk4UmrsQDXIvw51Sflqwb1FT6BYCe/sLyg/0H3yglkGID8A"
+      + "6mvCCHf9MSbyZt7Q36Mwcy7fc6UjMYq6YyIPFKn3JfRsXyKea1mngPGPLBXs76o2wEUQJBAPzzX8ik8h9lkYM6qn2"
+      + "KoZk4YcUEO7dNFCzlFdYb+k6OoMIA8Sa30XkJs49ZGnupq+9EXofql9NhByMZC3pLen0CQQCJT+7AIeHvACXyxcba"
+      + "5d3Ci02vNmKkhm6OrQ6F4xKNyh7663t6Q+RWr10uOonaUZ6YH4keuvyKWiMI+ztY5wgFAkAG8ohG8oDT6+47NHlKS"
+      + "Wx20N2ek6cwOaW8Ne6LmukdDz3LFkuJTLMsJ+AOp9vaWaanQ7F0+jSBUcDobd+q1DfhAkB3LJjayI1/EXHeMylT8w"
+      + "11O9JAr8MNaF+sFSb1rQ79YN9ih96zTxlu4uTMqqHaidxLy5MGyONGcNTXhrULg/jBAkEAgyFkw9l3+NbdW05ApnJ"
+      + "9S/UZ0Qwoi1wAhDDkjgACZwEkYEUy1I8oJMYyv/+Me8jqXo4fg9RJGNR9KNRRgBWbFg==";
 
-    //公钥加密
-    String publicStr = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCHrTlFCj92wUKGqoWG19iFFaUk/IkPRaVfRw"
-        + "Dh1r2OnO7bfrbSrblgAhkxeg9xcKFO99U4n1T4x2cjVkNdeVE7aKkCYvPSQGVidf6XzGkljZi0OMHn9DOl02PAP"
-        + "Cm5bOk+K8g9UdGmdNzleEU4MjiD01lNbebpRv/jQu8RPhhMcQIDAQAB";
-    String plainText = "12345678";
-    byte[] encryptBytes = RsaUtil
-        .encrypt(KeyType.PUBLICKEY, publicStr, plainText.getBytes());
-    String encryptStr = Base64.encodeBytes(encryptBytes);
-    System.out.println("1 encryptStr=[" + encryptStr + "]");
+  private String publicStr = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCHrTlFCj92wUKGqoWG19iFFaUk/IkP"
+      + "RaVfRwDh1r2OnO7bfrbSrblgAhkxeg9xcKFO99U4n1T4x2cjVkNdeVE7aKkCYvPSQGVidf6XzGkljZi0OMHn9DOl0"
+      + "2PAPCm5bOk+K8g9UdGmdNzleEU4MjiD01lNbebpRv/jQu8RPhhMcQIDAQAB";
 
-    //私钥解密
-    String privateStr = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAIetOUUKP3bBQoaqhYbX2IUVp"
-        + "ST8iQ9FpV9HAOHWvY6c7tt+ttKtuWACGTF6D3FwoU731TifVPjHZyNWQ115UTtoqQJi89JAZWJ1/pfMaSWNmLQ4"
-        + "wef0M6XTY8A8Kbls6T4ryD1R0aZ03OV4RTgyOIPTWU1t5ulG/+NC7xE+GExxAgMBAAECgYBoMl2IKx2gOz23JGB"
-        + "VtZDAbGYe6J7uDqO5b1M7HesICnfaNA997xMtq47jk4UmrsQDXIvw51Sflqwb1FT6BYCe/sLyg/0H3yglkGID8A"
-        + "6mvCCHf9MSbyZt7Q36Mwcy7fc6UjMYq6YyIPFKn3JfRsXyKea1mngPGPLBXs76o2wEUQJBAPzzX8ik8h9lkYM6q"
-        + "n2KoZk4YcUEO7dNFCzlFdYb+k6OoMIA8Sa30XkJs49ZGnupq+9EXofql9NhByMZC3pLen0CQQCJT+7AIeHvACXy"
-        + "xcba5d3Ci02vNmKkhm6OrQ6F4xKNyh7663t6Q+RWr10uOonaUZ6YH4keuvyKWiMI+ztY5wgFAkAG8ohG8oDT6+4"
-        + "7NHlKSWx20N2ek6cwOaW8Ne6LmukdDz3LFkuJTLMsJ+AOp9vaWaanQ7F0+jSBUcDobd+q1DfhAkB3LJjayI1/EX"
-        + "HeMylT8w11O9JAr8MNaF+sFSb1rQ79YN9ih96zTxlu4uTMqqHaidxLy5MGyONGcNTXhrULg/jBAkEAgyFkw9l3+"
-        + "NbdW05ApnJ9S/UZ0Qwoi1wAhDDkjgACZwEkYEUy1I8oJMYyv/+Me8jqXo4fg9RJGNR9KNRRgBWbFg==";
+  private String clearText = "1234567890杏花村abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      + "~！@#￥%……&*";
 
-    String unencryptStr = RsaUtil
-        .decrypt(KeyType.PUBLICKEY, privateStr, encryptBytes);
-    System.out.println("1 unencryptStr=[" + unencryptStr + "]");
-    Assert.assertEquals(unencryptStr, plainText);
+  private PrivateKey privateKey;
 
-    //私钥加密
-    encryptBytes = RsaUtil
-        .encrypt(KeyType.PRIVATEKEY, privateStr, plainText.getBytes());
-    encryptStr = Base64.encodeBytes(encryptBytes);
-    System.out.println("2 encryptStr=[" + encryptStr + "]");
+  private PublicKey publicKey;
 
-    //公钥解密
-    unencryptStr = RsaUtil.decrypt(KeyType.PRIVATEKEY, publicStr, encryptBytes);
-    System.out.println("2 unencryptStr=[" + unencryptStr + "]");
-    Assert.assertEquals(unencryptStr, plainText);
+  /**
+   * 初始化公钥和私钥.
+   */
+  @Before
+  public void init() throws Exception {
+    X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(Base64.decode(publicStr));
+    KeyFactory factory = KeyFactory.getInstance("RSA");
+    publicKey = factory.generatePublic(x509EncodedKeySpec);
+
+    PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(Base64.decode(privateStr));
+    privateKey = factory.generatePrivate(pkcs8EncodedKeySpec);
   }
 
 
+  @Test
+  public void testRsaString() {
+
+    String cipherText = RsaUtil.encrypt(KeyType.PUBLICKEY, publicStr, clearText);
+
+    Assert.assertEquals(clearText,
+        RsaUtil.decrypt(KeyType.PRIVATEKEY, privateStr, cipherText));
+
+  }
+
+  @Test
+  public void testRsaBytes() {
+    byte[] clearBytes = clearText.getBytes();
+
+    byte[] cipherBytes = RsaUtil.encrypt(KeyType.PRIVATEKEY, privateStr, clearBytes);
+
+    Assert.assertTrue(
+        Arrays.equals(clearBytes, RsaUtil.decrypt(KeyType.PUBLICKEY, publicStr, cipherBytes)));
+  }
+
+  @Test
+  public void testCharset() throws UnsupportedEncodingException {
+
+    String isoClearText = new String(clearText.getBytes(), "iso8859-1");
+
+    String isocipherText =
+        RsaUtil.encrypt(KeyType.PUBLICKEY, publicStr, isoClearText, "iso8859-1");
+
+    String isoresultClearText =
+        RsaUtil.decrypt(KeyType.PRIVATEKEY, privateStr, isocipherText, "iso8859-1");
+
+    isoresultClearText = new String(isoresultClearText.getBytes("iso8859-1"));
+
+    Assert.assertEquals(clearText, isoresultClearText);
+
+  }
+
+  @Test
+  public void testByRsaKey() {
+    byte[] cipherBytes = RsaUtil.encrypt(publicKey, clearText.getBytes());
+    byte[] clearBytes = RsaUtil.decrypt(privateKey, cipherBytes);
+
+    Assert.assertTrue(Arrays.equals(clearText.getBytes(), clearBytes));
+
+    cipherBytes = RsaUtil.encrypt(privateKey, clearText.getBytes());
+    clearBytes = RsaUtil.decrypt(publicKey, cipherBytes);
+
+    Assert.assertTrue(Arrays.equals(clearText.getBytes(), clearBytes));
+  }
 }
